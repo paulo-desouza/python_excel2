4fb61af2b3e6b4f23478e93a6bae795f0e8b2748
Paulo de Souza <paulodesouza.tech@gmail.com>
2023-02-09 11:55:18 -0500

    Finished Sorting tables by state.

--- growth_report.py
+++ growth_report.py
@@ -102,6 +102,7 @@ def write_table(client, worksheet, col_start=1):
     # write the tables.
 
     for i, key in enumerate(client):
+        
         char = get_column_letter(col_start)
         ws[char+str(i+1)].value = key
 
@@ -109,7 +110,7 @@ def write_table(client, worksheet, col_start=1):
             char = get_column_letter(col_start+j+1)
 
             if char == "A":
-                pass
+                pass 
             else:
                 ws[char+str(i+1)].value = client[key][j]
 
@@ -321,17 +322,21 @@ ws = wb["Timeline"]
 
 # OVERWRITE PAST REPORT'S TIMELINE
 col = 1
-for i, lst in enumerate(tables):
-    if i == 0:
-        pass 
-    else:
-        write_table(lst, ws, col)
-        col += 6
 
+f_tables = tables[:]
+f_tables.pop(0)
+
+
+for i, lst in enumerate(f_tables):
+    
+    write_table(lst, ws, col)
+    col += 6
+
+
+
+wb.save("result.xlsx")
 
 # get the filename, open the worksheet, loop for the data. 
-
-
 
 file_list = get_file_names()
 
@@ -358,11 +363,9 @@ for row in range(3, row_count+1):
             FIXED_INFO[(ws[char + str(row)].value)] = [ws[get_column_letter(column + 1) + str(row)].value, ws[get_column_letter(column + 2) + str(row)].value, ws[get_column_letter(column + 3) + str(row)].value]
             
 
-wb = load_workbook(sorted_datetimes[0])
+wb = load_workbook("result.xlsx")
 ws = wb["Timeline"]
             
-
-
 #Change positions of the worksheets. Bring current one to top.
 current_week_id = str(int(get_week_id(weeks_dt[1]))+1)
 
@@ -510,94 +513,158 @@ for k, state in enumerate(data_states):
                         check = 1
                     else:
                         the_data[k][5][item[0]] = item[1]
+
+
+# GET RE_ORGANIZED TITLES                        
+state_keys = []
                     
-
-
-# SCHOOL ID COLUMN ITERATION
-
-for i in range(0, len(titles)):
+for j, state in enumerate(the_data):
     
-    if i==0:
-        ws["A" + str(i+1)].value = titles[i]
+    for i, key in enumerate(state[5].keys()):
+        if i == 0:
+            state_keys.append([])
+        
+        state_keys[j].append(key)
+
+
+# DRAW THE TITLES ON THE TABLES
+
+counter = 1
+for j, state in enumerate(state_keys):
+    
+    ws["A" + str(counter)].value = data_states[j]
+    counter += 1
+    
+    for i in range(0, len(state)):
+    
+        ws["A" + str(counter)].value = state[i]
+        counter += 1
+
+
+
+counter = 1
+for j, state in enumerate(state_keys):
+    
+    ws["E" + str(counter)].value = data_states[j]
+    counter += 1
+    
+    for i in range(0, len(state)):
+    
+        ws["E" + str(counter)].value = state[i]
+        counter += 1
+    
+
+
+
+table_len = len(titles)      
+  
+counter = 1
+for j, state in enumerate(state_keys):
+    
+    ws["E" + str(counter + table_len + n_states + 4)].value = data_states[j]
+    counter += 1
+    
+    for i in range(0, len(state)):
+    
+        ws["E" + str(counter + table_len + n_states + 4)].value = state[i]
+        counter += 1
+    
+counter = 1
+for j, state in enumerate(state_keys):
+    
+    ws["A" + str(counter + table_len + n_states + 4)].value = data_states[j]
+    counter += 1
+    
+    for i in range(0, len(state)):
+    
+        ws["A" + str(counter + table_len + n_states + 4)].value = state[i]
+        counter += 1
+
+
+# DATA TABLE    
+
+counter = 1
+
+for j, data_ in enumerate(the_data):
+    if j == 0:
+        id_a = 0
     else:
-        ws["A" + str(i+1)].value = titles[i]
-
-table_len = len(titles)
-
-
-# DATA TABLE ITERATION
-for a, dic in enumerate(data):
-
-    for b, key in enumerate(dic):
-        
-        for col in range(2, 4):
-            char = get_column_letter(col)
+        id_a += len(the_data[j-1][5])+1
+   
+    
+    for a, dic in enumerate(data_):
+        b = 0  
+        for c, key in enumerate(dic):
             
-            if a == 1 and col == 2:
-                ws[char + str(b+1)].value = int(dic[key][0]/dic[key][3])
+        
+            for col in range(6, 11):
+                char = get_column_letter(col)
                 
+                if char == "F" and a == 1:
+                    ws[char + str(b+2+ id_a)].value = int(dic[key][0]/dic[key][3])
+                    ws[char + str(b+ id_a+table_len+ n_states + 6)].value = str(int((dic[key][0]/dic[key][3]) * 100 /dic[key][4]))+"%"
+                    ws[char + str(b+ id_a+table_len+ n_states + 6)].alignment = Alignment(horizontal = "right")
                 
-            elif a == 1 and col == 3:
-                try:
-                    ws[char + str(b+1)].value = int(dic[key][0]/dic[key][3]) - int(data[a-1][key][0]/dic[key][3])
-                except:
-                    pass
-                
-            elif a == 5 and char == "B":
-                ws[char + str(b+table_len + 4)].value = dic[key][4]
-        
-        
-        
-for i in range(0, len(titles)):
+                if char == "G" and a == 2:
+                    ws[char + str(b+2+ id_a)].value = int(dic[key][0]/dic[key][3])
+                    ws[char + str(b+ id_a+table_len+ n_states + 6)].value = str(int((dic[key][0]/dic[key][3]) * 100 /dic[key][4]))+"%"
+                    ws[char + str(b+ id_a+table_len+ n_states + 6)].alignment = Alignment(horizontal = "right")
+                    
+                if char == "H" and a == 3:
+                    ws[char + str(b+2+ id_a)].value = int(dic[key][0]/dic[key][3])
+                    ws[char + str(b+ id_a+table_len+ n_states + 6)].value = str(int((dic[key][0]/dic[key][3]) * 100 /dic[key][4]))+"%"
+                    ws[char + str(b+ id_a+table_len+ n_states + 6)].alignment = Alignment(horizontal = "right")
+                    
+                if char == "I" and a == 4:
+                    ws[char + str(b+2+ id_a)].value = int(dic[key][0]/dic[key][3])
+                    ws[char + str(b+ id_a+table_len+ n_states + 6)].value = str(int((dic[key][0]/dic[key][3]) * 100 /dic[key][4]))+"%"
+                    ws[char + str(b+ id_a+table_len+ n_states + 6)].alignment = Alignment(horizontal = "right")
+                    
+                if char == "J" and a == 5:
+                    ws[char + str(b+2+ id_a)].value = int(dic[key][0]/dic[key][3])
+                    ws[char + str(b+ id_a+table_len+ n_states + 6)].value = str(int((dic[key][0]/dic[key][3]) * 100 /dic[key][4]))+"%"
+                    ws[char + str(b+ id_a+table_len+ n_states + 6)].alignment = Alignment(horizontal = "right")
+            b += 1
+    #??? += len(data_[5])+1    
 
-    ws["E" + str(i+1)].value = titles[i]
 
-# DATA TABLE ITERATION
-for a, dic in enumerate(data):
-
-    for b, key in enumerate(dic):
+for j, data_ in enumerate(the_data):
+    
+    if j == 0:
+        id_a = 0
+    else:
+        id_a += len(the_data[j-1][5])+1
         
-        for col in range(6, 11):
-            char = get_column_letter(col)
+    for a, dic in enumerate(data_):
+        
+        b = 0
+    
+        for c, key in enumerate(dic):
             
-            if char == "F" and a == 1:
-                ws[char + str(b+1)].value = int(dic[key][0]/dic[key][3])
-                ws[char + str(b+table_len + 4)].value = str(int((dic[key][0]/dic[key][3]) * 100 /dic[key][4]))+"%"
-                ws[char + str(b+table_len + 4)].alignment = Alignment(horizontal = "right")
-            
-            if char == "G" and a == 2:
-                ws[char + str(b+1)].value = int(dic[key][0]/dic[key][3])
-                ws[char + str(b+table_len + 4)].value = str(int((dic[key][0]/dic[key][3]) * 100 /dic[key][4]))+"%"
-                ws[char + str(b+table_len + 4)].alignment = Alignment(horizontal = "right")
+            for col in range(2, 4):
+                char = get_column_letter(col)
                 
-            if char == "H" and a == 3:
-                ws[char + str(b+1)].value = int(dic[key][0]/dic[key][3])
-                ws[char + str(b+table_len + 4)].value = str(int((dic[key][0]/dic[key][3]) * 100 /dic[key][4]))+"%"
-                ws[char + str(b+table_len + 4)].alignment = Alignment(horizontal = "right")
-                
-            if char == "I" and a == 4:
-                ws[char + str(b+1)].value = int(dic[key][0]/dic[key][3])
-                ws[char + str(b+table_len + 4)].value = str(int((dic[key][0]/dic[key][3]) * 100 /dic[key][4]))+"%"
-                ws[char + str(b+table_len + 4)].alignment = Alignment(horizontal = "right")
-                
-            if char == "J" and a == 5:
-                ws[char + str(b+1)].value = int(dic[key][0]/dic[key][3])
-                ws[char + str(b+table_len + 4)].value = str(int((dic[key][0]/dic[key][3]) * 100 /dic[key][4]))+"%"
-                ws[char + str(b+table_len + 4)].alignment = Alignment(horizontal = "right")
+                if a == 1 and col == 2:
+                    ws[char + str(b+2+ id_a)].value = int(dic[key][0]/dic[key][3])
+                    
+                    
+                elif a == 1 and col == 3:
+                    try:
+                        ws[char + str(b+2+ id_a)].value = int(dic[key][0]/dic[key][3]) - int(data[a-1][key][0]/dic[key][3])
+                    except:
+                        pass
+                    
+                elif a == 5 and char == "B":
+                    ws[char + str(b+table_len+ id_a + n_states + 6)].value = dic[key][4]
+            b += 1
+    #??? += len(data_[5])+1      
         
-        
-        
-for i in range(0, len(titles)):
-
-    ws["A" + str(i+table_len + 4)].value = titles[i]
-
-        
-for i in range(0, len(titles)):
-
-    ws["E" + str(i+table_len + 4)].value = titles[i]
 
 
-                ###STYLE###
+
+
+
+ ###STYLE###
 
 for column in range(1, 25):
     for row in range(1, 60):
@@ -606,7 +673,7 @@ for column in range(1, 25):
         
 
 
-ws.move_range("A1:J" + str((table_len*2)+3), rows=3, cols=3)         #36 needs to be a variable that accounts for the number of schools
+ws.move_range("A1:J" + str(((table_len+n_states)*2)+4), rows=3, cols=3)         #36 needs to be a variable that accounts for the number of schools
 
 ws.row_dimensions[1].height = 24
 #ws.row_dimensions[table_len + 4].height = 24
@@ -624,15 +691,15 @@ ws["H1"].fill = PatternFill(fill_type='solid',
                             start_color='99ffcc',
                             end_color='99ffcc')
 
-ws["D"+str(table_len + 6)].font = Font(size = 19)
-ws["D"+str(table_len + 6)].alignment = Alignment(horizontal='center')
-ws["d"+str(table_len + 6)].fill = PatternFill(fill_type='solid',
+ws["D"+str(table_len+n_states + 7)].font = Font(size = 19)
+ws["D"+str(table_len+n_states + 7)].alignment = Alignment(horizontal='center')
+ws["d"+str(table_len+n_states + 7)].fill = PatternFill(fill_type='solid',
                             start_color='ccffcc',
                             end_color='ccffcc')
 
-ws["H"+str(table_len + 6)].font = Font(size = 19)
-ws["H"+str(table_len + 6)].alignment = Alignment(horizontal='center')
-ws["H"+str(table_len + 6)].fill = PatternFill(fill_type='solid',
+ws["H"+str(table_len+n_states + 7)].font = Font(size = 19)
+ws["H"+str(table_len+n_states + 7)].alignment = Alignment(horizontal='center')
+ws["H"+str(table_len+n_states + 7)].fill = PatternFill(fill_type='solid',
                             start_color='ccffcc',
                             end_color='ccffcc')
 
@@ -651,14 +718,14 @@ ws["I2"].font = Font(size = 8)
 
 ws.merge_cells("D1:F1")
 ws.merge_cells("H1:M1")
-ws.merge_cells("D"+str(table_len + 6)+":E"+str(table_len + 6))
-ws.merge_cells("H"+str(table_len + 6)+":M"+str(table_len + 6))
+ws.merge_cells("D"+str(table_len+n_states + 7)+":E"+str(table_len+n_states + 7))
+ws.merge_cells("H"+str(table_len+n_states + 7)+":M"+str(table_len+n_states + 7))
 
 ws["D1"].value = "FTE Children"
 
 ws["H1"].value = "FTE Children BD Projections"
-ws["D"+str(table_len + 6)].value = "School Max Capacity"
-ws["H"+str(table_len + 6)].value = "School Occupancy"
+ws["D"+str(table_len+n_states + 7)].value = "School Max Capacity"
+ws["H"+str(table_len+n_states + 7)].value = "School Occupancy"
 
 
 ws["E2"].value = "Current"
@@ -689,10 +756,11 @@ ws['B3'].alignment = Alignment(horizontal = "center", vertical = "center")
 
 ####BORDERS####
 set_border(ws, "B1:B3")
-set_border(ws, 'D1:F'+ str(table_len+3))        #ALL FOUR OF THESE ALSO NEED THE VARIABLES WITH THE AMOUNT OF SCHOOLS
-set_border(ws, 'D'+str(table_len + 6)+':E'+ str((table_len+3)*2)) 
-set_border(ws, 'H1:M'+ str(table_len+3)) 
-set_border(ws, 'H'+str(table_len + 6)+':M' + str((table_len+3)*2)) 
+set_border(ws, 'D1:F'+ str(table_len+n_states+3))        #ALL FOUR OF THESE ALSO NEED THE VARIABLES WITH THE AMOUNT OF SCHOOLS
+set_border(ws, 'D'+str(table_len +n_states+ 7)+':E'+ str((table_len+n_states+3)*2+1)) 
+set_border(ws, 'H1:M'+ str(table_len+n_states+3)) 
+set_border(ws, 'H'+str(table_len +n_states+ 7)+':M' + str((table_len+n_states+3)*2+1)) 
 
 
 wb.save("result.xlsx")
+
